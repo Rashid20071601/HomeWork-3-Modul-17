d4c87388e92884c31ac22d47921934fc5773ef5a
Rashid20071601 <153351715+Rashid20071601@users.noreply.github.com>
2025-01-03 17:44:33 +0300

    Add files via upload

---
+++ app/routers/task.py
@@ -0,0 +1,33 @@
+# Import
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/task', tags=['task'])
+
+
+
+# GET
+@router.get('/')
+async def all_tasks():
+  pass
+
+@router.get('/task_id')
+async def task_by_id():
+  pass
+
+
+# POST
+@router.post('/create')
+async def create_task():
+  pass
+
+
+# PUT
+@router.put('/update')
+async def update_task():
+  pass
+
+
+# DELETE
+@router.put('/delete')
+async def delete_task():
+  pass
---
+++ app/routers/user.py
@@ -0,0 +1,33 @@
+# Import
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/user', tags=['user'])
+
+
+
+# GET
+@router.get('/')
+async def all_users():
+  pass
+
+@router.get('/user_id')
+async def user_by_id():
+  pass
+
+
+# POST
+@router.post('/create')
+async def create_user():
+  pass
+
+
+# PUT
+@router.put('/update')
+async def update_user():
+  pass
+
+
+# DELETE
+@router.put('/delete')
+async def delete_user():
+  pass
